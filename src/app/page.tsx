4b73ef447588d0c611e0e6fb2484868172c5911a
alain <alain.haldi@pm.me>
2026-02-07 13:21:12 +0100

    Configure started landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <p>hello</p>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <p className="text-2xl">
+        This will soon become a project and task tracker app 🗂️
+      </p>
+    </div>
+  );
 }
