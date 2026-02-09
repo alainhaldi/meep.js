c87d130b0b4f40463516c74ff13d62cf43acbfd9
alain <alain.haldi@pm.me>
2026-02-09 19:50:03 +0100

    Add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import { LucideClipboardList } from "lucide-react";
+
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <p className="text-2xl">
-        This will soon become a project and task tracker app 🗂️
-      </p>
+    <div>
+      <div className=" h-12 flex items-center px-4 border-b">
+        <div className="flex gap-2">
+          <LucideClipboardList className="h-8" />
+          <p className="text-2xl font-bold">Tarea</p>
+        </div>
+      </div>
+      <div className="flex h-screen items-center justify-center">
+        <p className="text-2xl">
+          This will soon become a project and task tracker app 🗂️
+        </p>
+      </div>
     </div>
   );
 }
