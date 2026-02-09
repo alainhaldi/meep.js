3c8ebf092f7900ced8e65e118f975deb9c3fbcf9
alain <alain.haldi@pm.me>
2026-02-09 19:51:50 +0100

    Extract Header into its own component file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import { LucideClipboardList } from "lucide-react";
+import AppHeader from "@/components/header";
 
 export default function Home() {
   return (
     <div>
-      <div className=" h-12 flex items-center px-4 border-b">
-        <div className="flex gap-2">
-          <LucideClipboardList className="h-8" />
-          <p className="text-2xl font-bold">Tarea</p>
-        </div>
-      </div>
+      <AppHeader></AppHeader>
       <div className="flex h-screen items-center justify-center">
         <p className="text-2xl">
           This will soon become a project and task tracker app 🗂️
