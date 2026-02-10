27a93f731fab1b93284eefb3e9e80f0dcd76e5c7
alain <alain.haldi@pm.me>
2026-02-10 15:42:41 +0100

    Add Prisma Client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import AppProjectCard from "@/features/project/project-card";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const projects = await prisma.project.findMany();
+
   return (
     <div className="p-8 flex justify-center">
       <AppProjectCard></AppProjectCard>
