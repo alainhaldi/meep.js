2779e9a20bfc1f474402e6e5eca6cca99680fd50
alain <alain.haldi@pm.me>
2026-02-10 16:50:15 +0100

    Make the header sticky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const projects = await prisma.project.findMany();
 
   return (
-    <div className="p-8 flex flex-col gap-4">
+    <div className="p-4 flex flex-col gap-4">
       {projects.map((item) => (
         <div key={item.id} className="w-full flex justify-center">
           <AppProjectCard project={item} />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { LucideClipboardList } from "lucide-react";
 
 const AppHeader = () => {
   return (
-    <div className=" h-12 flex items-center px-4 border-b">
+    <div className="sticky top-0 z-50 h-12 flex items-center px-4 border-b bg-white">
       <div className="flex gap-2">
         <LucideClipboardList className="h-8" />
         <p className="text-2xl font-bold">Meep</p>
