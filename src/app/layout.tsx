974dbf04606df9c33e5a10737a0bf134e9bdf5a5
alain <alain.haldi@pm.me>
2026-02-17 20:19:34 +0100

    Implement Dark and Light Theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import AppHeader from "@/components/header";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,12 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AppHeader></AppHeader>
-        {children}
+        <ThemeProvider>
+          <AppHeader></AppHeader>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,14 @@
 import { LucideClipboardList } from "lucide-react";
+import { ThemeSwitcher } from "./theme/theme-switcher";
 
 const AppHeader = () => {
   return (
-    <div className="sticky top-0 z-50 h-12 flex items-center px-4 border-b bg-white">
+    <div className="sticky top-0 z-50 h-12 flex items-center justify-between px-4 border-b">
       <div className="flex gap-2">
         <LucideClipboardList className="h-8" />
         <p className="text-2xl font-bold">Meep</p>
       </div>
+      <ThemeSwitcher></ThemeSwitcher>
     </div>
   );
 };
