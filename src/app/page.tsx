d172d3438c989cd15abb80f6c5c2dbe1876c5b43
alain <alain.haldi@pm.me>
2026-02-18 22:18:54 +0100

    Add 5 sec delay testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+import { Spinner } from "@/components/spinner";
 import AppProjectCard from "@/features/project/project-card";
 import { getProjects } from "@/features/project/queries/get-projects";
 
@@ -6,11 +8,13 @@ export default async function Home() {
 
   return (
     <div className="p-4 flex flex-col gap-4">
-      {projects.map((item) => (
-        <div key={item.id} className="w-full flex justify-center">
-          <AppProjectCard project={item} />
-        </div>
-      ))}
+      <Suspense fallback={<Spinner />}>
+        {projects.map((item) => (
+          <div key={item.id} className="w-full flex justify-center">
+            <AppProjectCard project={item} />
+          </div>
+        ))}
+      </Suspense>
     </div>
   );
 }
--- src/features/project/queries/get-projects.ts
+++ src/features/project/queries/get-projects.ts
@@ -1,7 +1,15 @@
 import prisma from "@/lib/prisma";
 
 export const getProjects = async () => {
-  return await prisma.project.findMany({
-    orderBy: { title: "desc" },
-  });
+  const [projects] = await Promise.all([
+    prisma.project.findMany({
+      orderBy: { title: "desc" },
+    }),
+    new Promise((resolve) => setTimeout(resolve, 5000)),
+  ]);
+
+  return projects;
+  // return await prisma.project.findMany({
+  //   orderBy: { title: "desc" },
+  // });
 };
