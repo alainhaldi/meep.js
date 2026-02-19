9eba564c5d4fa9f79b4b18a103909274f268a68b
alain <alain.haldi@pm.me>
2026-02-19 20:23:24 +0100

    Remove project fetch delay

--- src/features/project/queries/get-projects.ts
+++ src/features/project/queries/get-projects.ts
@@ -1,15 +1,15 @@
 import prisma from "@/lib/prisma";
 
 export const getProjects = async () => {
-  const [projects] = await Promise.all([
-    prisma.project.findMany({
-      orderBy: { title: "desc" },
-    }),
-    new Promise((resolve) => setTimeout(resolve, 2000)),
-  ]);
+  // const [projects] = await Promise.all([
+  //   prisma.project.findMany({
+  //     orderBy: { title: "desc" },
+  //   }),
+  //   new Promise((resolve) => setTimeout(resolve, 2000)),
+  // ]);
 
-  return projects;
-  // return await prisma.project.findMany({
-  //   orderBy: { title: "desc" },
-  // });
+  // return projects;
+  return await prisma.project.findMany({
+    orderBy: { title: "desc" },
+  });
 };
