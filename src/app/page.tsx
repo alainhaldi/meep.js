51fdc98f38530bd68d9ffb943887119e6f79d22a
alain <alain.haldi@pm.me>
2026-02-19 22:27:26 +0100

    Add Project Bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import { Suspense } from "react";
 import { Spinner } from "@/components/spinner";
 import ProjectList from "@/features/project/components/project-list";
+import ProjectsBar from "@/features/project/components/projects-bar";
 
 export default async function Home() {
   return (
     // TODO: Add Custom Header
-    <Suspense fallback={<Spinner />}>
-      <ProjectList></ProjectList>
-    </Suspense>
+    <div className="bg-amber-50 ">
+      <ProjectsBar></ProjectsBar>
+      <Suspense fallback={<Spinner />}>
+        <ProjectList></ProjectList>
+      </Suspense>
+    </div>
   );
 }
---
+++ src/features/project/components/projects-bar.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const ProjectsBar = () => {
+  return (
+    <div className="flex flex-1 max-w-3xl items-center justify-between">
+      <p>ProjectBar</p>
+      <Button variant="outline">Add Project</Button>
+    </div>
+  );
+};
+
+export default ProjectsBar;
