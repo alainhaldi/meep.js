c80bf25fa84ef45093434e618b6d7d9ca8311ae6
alain <alain.haldi@pm.me>
2026-02-21 11:05:35 +0100

    Implement different Layout.tsx files

--- src/app/projects/(details)/project/layout.tsx
+++ src/app/projects/(details)/project/layout.tsx
@@ -1,13 +1,13 @@
 import AppHeader from "@/components/header";
 
-export default function ProjectsLayout({
+export default function ProjectDetailsLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <>
-      <AppHeader isHomeScreen={true}></AppHeader>
+      <AppHeader isHomeScreen={false}></AppHeader>
       <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/10 flex flex-col">
         {children}
       </main>
